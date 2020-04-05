93a41bcac8bc2bd61af3292b87fa1ace0a9c6b92
Alex Luckerman <alex.luckerman@gmail.com>
2020-04-04 19:04:59 -0500

    Add auth in-progress

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,11 +1,12 @@
+const secrets = require('./secrets.json');
 const passport = require('passport');
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: "",
-    clientSecret: "",
-    callbackURL: "http://localhost:1919"
+    clientID: secrets.google-client-id,
+    clientSecret: secrets.google-client-secret,
+    callbackURL: "http://localhost:1919/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
